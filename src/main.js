fda74df9361788efae3a8c5ed4ec21118990a1d5
Leonhard Bauer <leonhard_bauer@mailbox.org>
2021-10-15 17:56:58 +0200

    implement pwa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import App from './App.svelte';
 
+window.onload = () => {
+	'use strict';
+  
+	if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker
+			   .register('./sw.js');
+	}
+  }
+
 const app = new App({
 	target: document.body
 });
